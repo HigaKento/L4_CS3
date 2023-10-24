0ba0567ab0d555a02b14fa30f1ab674ab8e0169e
Kento Higa <2111140057r@ed.fuk.kindai.ac.jp>
2023-10-24 16:44:56 +0000

    first commit

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -6,16 +6,27 @@ class TopController < ApplicationController
       render "login"
     end
   end
-    
+  
+  def create
+  end
+
+  def create2
+    signup_password = BCrypt::Password.create(params[:pass])
+    User.create(uid: params[:uid], pass: signup_password)
+    redirect_to top_main_path
+  end
+
   def login
-    if User.find_by(uid: params[:uid]) and User.find_by(pass: params[:pass])
+    user = User.find_by(uid: params[:uid])
+    login_password = BCrypt::Password.new(user.pass)
+    if login_password ==params[:pass]
       session[:login_uid]=params[:uid]
       redirect_to top_main_path
     else
       render "error"
     end
   end
-    
+
   def logout
     session.delete(:login_uid)
     redirect_to top_main_path
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   post 'top/login'
   root "top#main"
   get 'top/logout'
+  get 'top/create'
+  post 'top/create2'
 end
